85cd6bc976169965b43c0bf6d78f5074d4b7c744
Andres2626 <andrescamorao9@gmail.com>
2025-08-12 15:39:31 -0500

    12-08-25 [patch 2] Deleted build counter system

--- Core/update_counter.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-setlocal enabledelayedexpansion
-set file=build.h
-set /p line=<%file%
-for /f "tokens=3" %%a in ("%line%") do set /a count=%%a + 1
-echo #define BUILD_COUNT !count! > %file%
